91da8f9130c314dc4bd4b8811355327f21caa6a2
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-09-21 21:04:28 -0400

    Entidades

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace DAL
 {
     public class Contexto : DbContext
     {
-        public DbSet<Usuario> usuario { get; set; }
+        public DbSet<Usuarios> Usuario { get; set; }
 
         public Contexto() : base("ConStr")
         { }
---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    [Serializable]
+    public class Usuarios
+    {
+       
+        [Key]
+        public int UsuariosId { get; set; }
+
+        public Usuarios(int usuariosId)
+        {
+            this.UsuariosId = usuariosId;
+        }
+    }
+}
