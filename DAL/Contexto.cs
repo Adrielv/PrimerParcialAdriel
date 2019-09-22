31ef4552cff25aa8d36f89a4dfb63f1bd568d36f
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-09-21 20:49:20 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuario> usuario { get; set; }
+
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
